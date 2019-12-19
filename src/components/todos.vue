3da891b38d15b02780cf31c833e8b30868e78a77
abdulmalikgh <50835233+abdulmalikgh@users.noreply.github.com>
2019-12-19 14:39:21 -0800

    added css

---
+++ src/components/todos.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h3>Todos</h3>
+        <div class="todos">
+            <div class="todo" v-for="todo in allTodos" :key="todo">
+              {{todo.title}}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import {mapGetters } from 'vuex';
+export default {
+    name: "Todos",
+    computed:mapGetters(['allTodos'])
+}
+</script>
+
+<style scoped>
+.todo {
+    border:1px solid #ccc;
+    background: #41b883;
+    padding:1rem;
+    border-radius: 5px;
+    text-align:center;
+    position:relative;
+    cursor: pointer;
+}
+</style>
